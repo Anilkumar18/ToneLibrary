6468147a379d8f6bc27830f35f77e2f2f6b47021
IgnesiousRobin <32846534+Anilkumar18@users.noreply.github.com>
2024-03-28 17:38:42 +0530

    Commits for TestToneLibrary

--- Package.swift
+++ Package.swift
@@ -14,25 +14,15 @@ let package = Package(
             targets: ["ToneListenWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AudioKit/AudioKit.git", .upToNextMajor(from: "5.3.0")),
-        .package(url: "https://github.com/AudioKit/SoundpipeAudioKit.git", .upToNextMajor(from: "5.3.0")),
-        .package(url: "https://github.com/AudioKit/DunneAudioKit.git", .upToNextMajor(from: "5.3.0")),
-        .package(url: "https://github.com/AudioKit/STKAudioKit.git", .upToNextMajor(from: "5.3.0")),
-//        .package(url: "https://github.com/AudioKit/AudioKitUI.git", from: "5.2.3"),
-        .package(url: "https://github.com/zoul/generic-json-swift", from: "2.0.1"),
-        .package(url: "https://github.com/malcommac/SwiftLocation", from: "5.1.0"),
+        .package(url: "https://github.com/AudioKit/AudioKit.git", .exactItem("5.3.0")),
+        .package(url: "https://github.com/malcommac/SwiftLocation", .exactItem("5.1.0"))
     ],
     targets: [
         .target(
                     name: "ToneListenWrapper",
                     dependencies: [
                         .product(name: "AudioKit", package: "AudioKit"),
-                        .product(name: "SoundpipeAudioKit", package: "SoundpipeAudioKit"),
                         .product(name: "SwiftLocation", package: "SwiftLocation"),
-//                        .product(name: "AudioKitUI", package: "AudioKitUI"),
-                        .product(name: "DunneAudioKit", package: "DunneAudioKit"),
-                        .product(name: "STKAudioKit", package: "STKAudioKit"),
-                        .product(name: "GenericJSON", package: "generic-json-swift"),
                         "ToneListen"
                     ],
                     path: "Sources/Wrapper",
